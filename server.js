ac35dc58922aeba7d0a8e5bae2ec5901c5fd3bfc
sidonie <shelliednnguyen@gmail.com>
2021-03-02 12:36:40 -0800

    start server and db connection

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const morganLogger = require( 'morgan' );
 
+// Verobse produces messages in the terminal regarding the state of the runtime,
+// which can help explain what the app is doing, specifically SQLite.
+const sqlite3 = require( 'sqlite3' ).verbose();
+
 // Setup Express middleware to json parse and urlendoded for POST requests.
 app.use( express.urlencoded({ extended: false }));
 app.use( express.json());
@@ -13,21 +17,32 @@ app.use( morganLogger( 'dev' ));
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// Test route, remove later
-/* app.get( '/', ( req, res ) => {
-   res.json({ message: 'Hello World' });
-}); */
+// Connect the application to the SQLite database by creating a new instance
+// of the election.db.
+const db = new sqlite3.Database( './db/election.db', err => {
+   if ( err ) {
+      return console.error( err.message );
+   };
+
+   console.log( 'Connected to the election database.' );
+});
 
 
 // Route to handle user requets that aren't supported by the app,
-// any other request (Not Found) catch all.
+// (Not Found) catch all.
 // Make sure this route is placed as the last route.
 app.use(( req, res ) => {
    res.status( 404 ).end();
 });
 
 
-// Start Express.js server on designated port.
-app.listen( PORT, () => {
-   console.log( `Server running on port ${PORT}` );
+// Start server after DB connection.  This is to ensure that the
+// Express.js server doesn't start before the connection to the
+// database has been established, hence we wrp the Express.js erver
+// connection in an event handler.
+db.on( 'open', () => {
+   // Start Express.js server on designated port.
+   app.listen( PORT, () => {
+      console.log( `Server running on port ${PORT}` );
+   });
 });
