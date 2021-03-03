3c1fed7301c506b6a4ab043b67f60703c1810aaf
sidonie <shelliednnguyen@gmail.com>
2021-03-02 23:22:21 -0800

    party update

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const db = new sqlite3.Database( './db/election.db', err => {
    console.log( 'Connected to the election database.' );
 });
 
+
 /* GEt all candidates
 The all() method runs an SQL query and executes the callback with all
 the resulting rows that match the query.  This method allows SQL commands
@@ -142,6 +143,92 @@ app.post( '/api/candidate', ({ body }, res ) => {
          // lastID is the id of the inserted row.
          id: this.lastID
       });
+   });
+});
+
+
+// Update a candidate.
+app.put( '/api/candidate/:id', ( req, res ) => {
+
+   // Verify that party_id was provided before attempt to update the db.
+   const errors = inputCheck( req.body, 'party_id' );
+
+   if ( errors ) {
+      res.status( 400 ). json({ error: errors });
+      return;
+   };
+   
+   const sql = `UPDATE candidates SET party_id = ?
+                WHERE id = ?`;
+   // Value to update with is in the body and value to check against
+   // for update is in the params.
+   const params = [ req.body.party_id, req.params.id ];
+
+   db.run( sql, params, function( err, result ) {
+      if ( err ) {
+         res.status( 400 ).json({ error: err.message });
+         return;
+      };
+
+      res.json({
+         message: 'success',
+         data: req.body,
+         changes: this.changes
+      });
+   });
+});
+
+
+// Get all parties.
+app.get( '/api/parties', ( reg, res ) => {
+   const sql =   `SELECT * FROM parties`;
+   const params = [];
+
+   db.all( sql, params, ( err, rows ) => {
+      if ( err ) {
+         res.status( 500 ).json({ error: err.message });
+         return;
+      };
+
+      res.json({
+         message: 'success',
+         data: rows
+      });
+   });
+});
+
+
+// Get a single party.
+app.get( '/api/party/:id', ( req, res ) => {
+   const sql = `SELECT * FROM parties WHERE ID = ?`;
+   const params = [ req.paramsid ];
+
+   db.get( sql, params, ( err, row ) => {
+      if ( err ) {
+         res.status( 400 ).json({ error: err.message });
+         return;
+      };
+
+      res.json({
+         message: 'success',
+         data: row
+      });
+   });
+});
+
+
+// Date a party
+app.delete( '/api/party/:id', ( req, res ) => {
+   const sql = `DELETE FROM parties WHERE id = ?`;
+   const params = [ req.params.id ];
+
+   db.run( sql, params, function( err, result ) {
+      if ( err ) {
+         res.status( 400 ).json({ error: res.message });
+         return;
+      };
+
+      res.json({ message: 'successfully deleted', changes: this.changes });
    });
 });
 
